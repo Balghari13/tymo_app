6191c7bc15920b87b7e70df5680feb521b436de5
Balghari <najmbalghari13@gmail.com>
2023-08-26 12:12:06 +0500

    added initial login screen

--- lib/Screens/Auth/login.dart
+++ lib/Screens/Auth/login.dart
@@ -1,117 +1,105 @@
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
-import 'package:tymo_app/Screens/Auth/forget_password.dart';
-import 'package:tymo_app/Screens/Auth/registration.dart';
-import 'package:tymo_app/Screens/widgets/custom_heading.dart';
-import 'package:tymo_app/Screens/widgets/custom_input.dart';
+
 
 class LogInScreen extends StatelessWidget {
   LogInScreen({super.key});
 
   final RoundedLoadingButtonController _btnController = RoundedLoadingButtonController();
-
+void logIn(){
+  print('logging...');
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Log In',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          style: TextStyle(fontWeight: FontWeight.bold,
+              fontSize: 24, fontFamily: 'Poppins'),
         ),
         centerTitle: true,
-        backgroundColor: Color(0xff000211),
+        backgroundColor: const Color(0xff000211),
       ),
       body: Padding(
         padding: const EdgeInsets.all(40.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 25,),
-            CustomHeadings(mainText: 'Sign In to Tymo',
-                subText: 'To connect with \n your partner',
-                logo: 'assets/ty.png', fontSize: 25, logoSize: 10),
-            SizedBox(height: 15,),
-            CustomInput(keyboardType: TextInputType.emailAddress,
-                labelText: 'Enter Email',
-                obsecureText: false, icon: Icons.email,
-                size: 18),
-            // TextField(
-            // decoration: InputDecoration(
-            //   filled: true,
-            //   fillColor: Colors.grey,
-            //   border: OutlineInputBorder(
-            //     borderRadius: BorderRadius.circular(15),
-            //     borderSide: BorderSide.none,
-            //   ),
-            //
-            //   prefixIcon: Icon(Icons.email, size: 18,),
-            //   prefixIconColor: Colors.green,
-            //   label: Text('Enter your Email'),
-            //   floatingLabelBehavior: FloatingLabelBehavior.never,
-            // ),
-            //   keyboardType: TextInputType.emailAddress,
-            //   obscureText: false,
-            //   autofocus: false,
-            //   textAlign: TextAlign.start,
-            //   textAlignVertical: TextAlignVertical.center,
-            // ),
-            SizedBox(height: 15,),
-            CustomInput(keyboardType: TextInputType.text,
-                labelText: "Enter Password", obsecureText: true,
-                icon: Icons.password,
-                size: 18,),
-            // TextField(
-            //   decoration: InputDecoration(
-            //     fillColor: Colors.grey,
-            //     filled: true,
-            //     prefixIcon: Icon(Icons.password),
-            //     prefixIconColor: Colors.green,
-            //     label: Text('Password'),
-            //     labelStyle: TextStyle(color: Colors.white, fontSize: 18),
-            //     floatingLabelBehavior: FloatingLabelBehavior.never,
-            //     border: OutlineInputBorder(
-            //       borderRadius: BorderRadius.circular(15),
-            //       borderSide: BorderSide.none,
-            //     ),
-            //   ),
-            //   obscureText: true,
-            //   autofocus: false,
-            //
-            // ),
-SizedBox(height: 15,),
+            Row(
+              children: [
+                const Text('Sign in to Tympo'),
+                Image.asset('assets/ty.png', height: 20, width: 20,),
+              ],
+            ),
+            const Text('To connect with \n your partner'),
+
+            const SizedBox(height: 15,),
+            TextField(
+              textAlign: TextAlign.start,
+              textInputAction: TextInputAction.none,
+              obscureText: false,
+              autofocus: false,
+              keyboardType: TextInputType.emailAddress,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                //hintText: 'Email',
+                fillColor: Colors.grey.shade300,
+                filled: true,
+                label: const Text('Email'),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                prefixIcon: const Icon(Icons.email),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10.0),
+                )
+              ),
+              style: const TextStyle(fontFamily: 'Poppins'),
+            ),
+            const SizedBox(height: 15,),
+            TextField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "password",
+                prefixIcon: const Icon(Icons.password),
+                filled: true,
+                fillColor: Colors.grey.shade300,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10.0),
+                )
+              ),
+              style: const TextStyle(fontFamily: 'Poppins'),
+            ),
+            const SizedBox(height: 15,),
+
+
             RoundedLoadingButton(
-              width: double.infinity,
+                width: double.infinity,
                 borderRadius: 10,
                 color: Colors.green,
-                controller: _btnController, onPressed: (){
-            },
-                child: Text('Log In')),
-            SizedBox(height: 15,),
-            Align(
-              alignment: Alignment.centerLeft,
-                child: InkWell(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ForgetPassword()));
-                  },
-                  child: Text('Forget Password?', style: TextStyle(color: Colors.redAccent),
-                  ),
-                ),),
-            
+                controller: _btnController,
+                onPressed: logIn,
+                child: const Text('Log In')),
+            const SizedBox(height: 15,),
+            const Text('Forget Password', style: TextStyle(color: Colors.red),),
+            const SizedBox(height: 15,),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Don't have an account?"),
-                TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>RegistrationScreen()));
-                }, child: Text('Sign Up'))
+                Text("Don't have ab account?"),
+                Text('Sign Up.', style:
+                TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),)
               ],
             )
 
 
-
           ],
         ),
-      ),
+      )
     );
   }
 }
